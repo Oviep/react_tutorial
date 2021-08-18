f80c7a1eded1e10d73e0a8b00e0d3e364e2c5284
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2021-08-18 14:55:12 +0100

    provided functionto display form or to hide it. Added components for the Chart bars to display the expenses for each cost added to the expense list

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
-import ExpenseForm from "./ExpenseForm";
+import React, {useState} from 'react'
+import ExpenseForm  from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing]=useState(false)
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -10,9 +12,21 @@ const NewExpense = (props) => {
         };
 
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
+    const startEditingHandler = () => {
+            setIsEditing(true)
+    }
+
+
     return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+       {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>} 
+       {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>} 
     </div>
 }
 
